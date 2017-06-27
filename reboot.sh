c4a8bb26d1c95024c7b4448026eb6c90d011e0b8
Maria <maria@debian>
2017-06-27 12:01:26 +0200

    Add reboot script

---
+++ reboot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IP_FILE="$1"
+
+IPS=()
+
+while IFS='' read -r line || [[ -n "${line}" ]]; do
+    IPS+=(${line})
+done < "${IP_FILE}"
+
+for IP in "${IPS[@]}"
+do
+    sshpass -p root ssh -o StrictHostKeyChecking=no -t root@${IP} ash -c "'reboot'"
+done
